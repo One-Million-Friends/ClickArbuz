c6d4336e544ff06cd2c34a5a3b81f555b7618b29
Vladislav Polyakov <me@polrk.com>
2024-05-11 07:30:39 +0300

    fix: ignore errors when auto upgrading

--- upgrade.ts
+++ upgrade.ts
@@ -1,6 +1,7 @@
 import {BunRuntime} from "@effect/platform-bun";
 import {ConfigProvider, Effect, pipe, Schedule} from "effect";
-import {getBoosts, upgradeBoost} from "./game/api.ts";
+import {constVoid} from "effect/Function";
+import {getBoosts, getProfile, upgradeBoost} from "./game/api.ts";
 import {Telegram} from "./telegram/client.ts";
 
 const upgrade = Effect.gen(function* (_) {
@@ -18,21 +19,35 @@ const upgrade = Effect.gen(function* (_) {
         return Effect.none
     }
 
+    const profile = yield* getProfile(tgWebAppData)
+    if (profile.banned) {
+        console.log("🛑 BANNED!!!")
+        return Effect.none;
+    }
+
+    console.log("🤠PROFILE:", profile.fullName, "💎", profile.clicks)
+
     return yield* Effect.repeat(Effect.gen(function* (_) {
         const boosts = yield* getBoosts(tgWebAppData)
-        const sorted = boosts.sort((a, b) => a.pfs - b.pfs)
+        const sorted = boosts
+            // .filter((boost) => boost.type === "MINER")
+            .sort((a, b) => a.pfs - b.pfs)
         const best = sorted[0]
 
         yield* upgradeBoost(tgWebAppData, best.id)
-
         console.log("✅UPGRADED:", best.name, best.level, "=>", best.level + 1)
-
-        yield* Effect.sleep("10 millis")
     }), Schedule.forever)
 })
 
+const policy = Schedule.addDelay(Schedule.forever, () => "5 minutes")
+
+const program = Effect.match(upgrade, {
+    onFailure: constVoid,
+    onSuccess: constVoid
+})
+
 pipe(
-    upgrade,
+    Effect.repeat(program, policy),
     Effect.provide(Telegram.live),
     Effect.withConfigProvider(ConfigProvider.fromEnv()),
     BunRuntime.runMain
